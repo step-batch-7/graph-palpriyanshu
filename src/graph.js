d2e0e92ce561717833e53224c83eb7e7f3f2ed39
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-06-10 22:29:39 +0530

    modified bfs function and add parseList

--- src/graph.js
+++ src/graph.js
@@ -1,6 +1,27 @@
-
+const parseList = function(list){
+  const relations = {}
+  list.forEach(([source, destination]) => {
+    if(!relations.hasOwnProperty(source)){
+      relations[source] = [];
+    }
+    relations[source].push(destination);
+  });
+  return relations;
+}
 const bfs = function(pairs,source,target){
-  
+  const paths = parseList(pairs);
+  let toVisit = [source];
+  const visited = [];
+  while(toVisit.length){
+    const node = toVisit.shift();
+    if(node == target){
+      return true;
+    }
+    visited.push(node);
+    if(paths[node]){
+      toVisit = toVisit.concat(paths[node].filter(n => toVisit && !toVisit.includes(n) && !visited.includes(n)));
+    }
+  }
+  return false;
 };
-
 module.exports = {bfs};

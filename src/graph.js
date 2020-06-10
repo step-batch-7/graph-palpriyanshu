78ffbf7a398890a807316d135415f6052f69f6db
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-06-11 00:55:54 +0530

    modified bfs and added test for missing source node

--- src/graph.js
+++ src/graph.js
@@ -10,21 +10,17 @@ const parseList = function(list){
 }
 
 const bfs = function(pairs,source,target){
-  if(pairs.length == 1 && source == target && pairs[0][0] != pairs[0][1]){
-    return false;
-  }
   const paths = parseList(pairs);
   let toVisit = [source];
   const visited = [];
   while(toVisit.length){
     const node = toVisit.shift();
-    if(node == target){
+    const neighbours = paths[node] || [];
+    if(neighbours.includes(target)){
       return true;
     }
     visited.push(node);
-    if(paths[node]){
-      toVisit = toVisit.concat(paths[node].filter(n => !toVisit.includes(n) && !visited.includes(n)));
-    }
+    toVisit = toVisit.concat(neighbours.filter(n => !toVisit.includes(n) && !visited.includes(n)));
   }
   return false;
 };
--- test/testBfs.js
+++ test/testBfs.js
@@ -5,7 +5,7 @@ const {bfs} = require("../src/graph");
 describe("BFS",function() {
   const pairs = [["India", "Pakistan"],["India","Nepal"],["India", "China"],["Nepal","China"],["Pakistan","China"]];
 
-  it("should give true if source has path to target", function(){
+  it("should give true if two node connected perfectly", function(){
     assert.ok(bfs(pairs, "India","Pakistan"))
   })
 
@@ -18,8 +18,13 @@ describe("BFS",function() {
     assert.ok(bfs(routes, "aa","aa"))
   })
 
-  it("should give true for single node not connected to itself", function(){
+  it("should give false for single node not connected to itself", function(){
     const routes = [["aa","bb"]];
     assert.notOk(bfs(routes, "aa","aa"))
+  })
+
+  it("should give false for two nodes not connected", function(){
+    const routes = [["aa","bb"],["aa","cc"]];
+    assert.notOk(bfs(routes, "dd","cc"))
   })
 });
